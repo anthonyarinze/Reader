03bfe8163b4e141861a7186104b1371ea2649e68
Anthony Arinze <anthonyarinze28@gmail.com>
2022-12-05 19:20:59 +0100

    Final Commit (Likely ;)

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:reader/categories.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,8 +29,8 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Material(
-      child: Column(
+        child: Scaffold(
+      body: Column(
         children: [
           //Appbar Buttons
           Padding(
@@ -104,6 +106,7 @@ class _HomePageState extends State<HomePage>
             ),
           ),
 
+          //Popular Books & See All
           Padding(
             padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
             child: Row(
@@ -124,8 +127,82 @@ class _HomePageState extends State<HomePage>
               ],
             ),
           ),
+
+          //Books
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                BuildBookCover(image: 'images/1.jpg', title: "Art Of War"),
+                BuildBookCover(
+                    image: 'images/2.jpg', title: "The Obstacle Is The Way"),
+                BuildBookCover(image: 'images/3.jpg', title: "Atomic Habits"),
+                BuildBookCover(
+                    image: 'images/4.jpg',
+                    title: "The 7 Habits Of Highly Effective People"),
+                BuildBookCover(
+                    image: 'images/5.jpg', title: "Man's Search For Meaning"),
+                BuildBookCover(image: 'images/6.jpg', title: "Ikigai"),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+            child: Container(
+              width: double.infinity,
+              height: 180,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: <Color>[Colors.pinkAccent, Colors.purpleAccent]),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     ));
+  }
+}
+
+class BuildBookCover extends StatelessWidget {
+  late String image;
+  late String title;
+
+  BuildBookCover({
+    Key? key,
+    required this.image,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+      child: Column(
+        children: [
+          Image.asset(
+            image,
+            fit: BoxFit.cover,
+            height: 180,
+            width: 120,
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 0.0),
+            width: 120,
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
